8fe52edd87028ada7bec66a6aa945e8794d40b95
Gerardo <gsanchezc1211@gmail.com>
2017-12-07 16:31:19 -0500

    add pods

--- InfiniteScroll/ViewController.swift
+++ InfiniteScroll/ViewController.swift
@@ -8,18 +8,49 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var table: UITableView!
+    var labels = ["1","2","3","4","5","6","7","8","9","10"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        table.delegate = self
+        table.dataSource = self
+        
+        table.addInfiniteScroll { (result) in
+            // todo tu código
+            self.labels = self.labels + self.labels
+            self.table.reloadData()
+            //finalizar el scroll infinito
+            self.table.finishInfiniteScroll()
+        }
+        //Carga 500 ptos antes de llegar al final del scroll.
+        table.infiniteScrollTriggerOffset = 500;
+    }
+}
+
+extension ViewController:UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+}
+
+extension ViewController:UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return labels.count
     }
 
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = labels[indexPath.row]
+        return cell
+    }
 }
 
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'InfiniteScroll' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+	pod 'UIScrollView-InfiniteScroll'
+
+  # Pods for InfiniteScroll
+
+end
